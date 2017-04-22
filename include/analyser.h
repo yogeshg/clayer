1b626771acf13d3bdf3c205998808c82b8bd9296
Yogesh Garg <yogeshg91@gmail.com>
2017-04-22 01:40:48 -0400

    refactoring int to log_properties for type safety

--- include/analyser.h
+++ include/analyser.h
@@ -9,24 +9,24 @@ namespace clayer {
     namespace analyser {
 
         class Parser {
-            std::vector<Property> records;
+            std::vector<LogRecord> records;
         public:
 
-            const std::vector<Property> read_file(std::string filename) {
+            const std::vector<LogRecord> read_file(std::string filename) {
                 records.clear();
                 std::ifstream f(filename);
                 for(std::string line; std::getline(f, line); ) {
-                    Property p;
+                    LogRecord p;
                     // std::cout << p <<" -- " << line << "\n";
-                    parse_props<clayer::FILE, clayer::FUNC, clayer::LINE>(p, line);
+                    parse_props<clayer::FILE, clayer::FUNC, clayer::LINE, clayer::MESG>(p, line);
                     // std::cout << p <<" -- " << line << "\n";
                     records.push_back(p);
                 }
                 return records;
             }
 
-            std::set<Property::State> get_states() {
-                std::set<Property::State> states;
+            std::set<LogRecord::State> get_states() {
+                std::set<LogRecord::State> states;
                 for(auto r : records) {
                     states.insert(r.get_state());
                 }
