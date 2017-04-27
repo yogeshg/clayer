7f43c022a56d46ef406f662a1aa3edc9ff18c916
Andy Lianghua Xu <lx2180@columbia.edu>
2017-04-27 00:47:38 -0400

    stresstest.sh to grep the mean response time per HTTP request

--- test/stresstest.sh
+++ test/stresstest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <server_address:port_num>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <request_count> <simultaneous_connection> <server_address:port_num> "
     exit
 fi
 SERVERADDR=$1
 
-ab -n 500 -c 25  -H "Accept-Encoding: gzip, deflate" http://$1/work # Run GETWORK/multithreading test
+ab -n $1 -c $2  -H "Accept-Encoding: gzip, deflate" http://$3/work | grep "Time per request"
 
 
