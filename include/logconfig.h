9c4a0e5fa01db4dcfc0c92f810357d25c0bf763e
Steven <ys2833@columbia.edu>
2017-04-26 21:23:09 -0400

    thread output

--- include/logconfig.h
+++ include/logconfig.h
@@ -1,8 +1,11 @@
 #ifndef __LOGCONFIG_H__
 #define __LOGCONFIG_H__
 
+#include <chrono>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <thread>
 
 #include "logger.h"
 
@@ -16,9 +19,23 @@ enum Severity {
   CRITICAL = 50
 };
 
-constexpr const char format[] = "\033[1;31m[%]\033[0m %(%:%): [%]";
-using MainLogger = Logger<DEBUG, std::ostream, std::clog, format,
-                          prop_time, prop_file, prop_func, prop_line, prop_msg>;
+void prop_time(std::ostream &o, const ContextInfo &i) {
+  using std::chrono::system_clock;
+  auto now = system_clock::to_time_t(system_clock::now());
+  o << std::put_time(std::localtime(&now), "%F %T");
+}
+
+void prop_thread(std::ostream &o, const ContextInfo &i) {
+  auto f = o.flags();
+  o << std::hex << std::showbase << "Thread " << std::hex << std::showbase
+    << std::this_thread::get_id();
+  o.flags(f);
+}
+
+constexpr const char format[] = "\033[1;31m[%]\033[0m % %(%:%): [%]";
+using MainLogger =
+    Logger<DEBUG, std::ostream, std::clog, format,
+           prop_time, prop_thread, prop_file, prop_func, prop_line, prop_msg>;
 
 // Example extended logger
 // void prop_ip(std::ostream &o, const ContextInfo &i) { o << "127.0.0.1"; }
