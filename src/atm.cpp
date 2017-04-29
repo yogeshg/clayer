9e12b7f29b4c3662475271b463f76de813d4a80e
Yogesh Garg <yogeshg91@gmail.com>
2017-04-29 13:33:54 -0400

    code for example used in slides

--- src/atm.cpp
+++ src/atm.cpp
@@ -10,18 +10,24 @@ void withdraw(int &account) {
   for (int i = 0; i < iterations; i++) {
     account--;
     LOG(WARNING) << "Balance after withdraw: " << account
-    << " logging a random integer " << 1000+rand()%100;
-    usleep(i);
+      << " logging a random integer " << 1000+rand()%100;
+    // sleep proportional to (t-mid)^2
+    usleep([](float x){return (x*x)/10;}(i-(iterations/2)));
   }
 }
 
 void deposit(int &account) {
-  for (int i = 0; i < iterations; i++) {
-    account++;
-    LOG(INFO) << logger::hash::off
-              << "Balance after deposit: " << logger::hash::on << account
-              << " logging a random integer " << 1000+rand()%100;
-    usleep(iterations-i);
+  for (int i = 0; i < 2*iterations; i++) {
+    const int delta = std::max((10001-account)/2, 0);
+    LOG(DEBUG) << "Check";
+    if(delta){
+      account+=delta;
+      LOG(INFO) << logger::hash::off
+        << "Balance after deposit: " << logger::hash::on << account
+        << " logging a random integer " << 1000+rand()%100;
+    }
+    // sleep for an average of n^2
+    usleep(iterations*iterations/10);
   }
 }
 
