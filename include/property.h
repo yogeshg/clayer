a0445de9bfb0b4988ab06a85e2b36ec0f06e84a6
Yogesh Garg <yogeshg91@gmail.com>
2017-04-29 14:45:33 -0400

    documenting property

--- include/property.h
+++ include/property.h
@@ -1,3 +1,6 @@
+/**
+ * Classes and functions useful for Log Properties
+ */
 #ifndef __PROPERTY_H__
 #define __PROPERTY_H__
 
@@ -19,23 +22,29 @@ template <typename T> concept bool StringRef = requires(T o) {
   { *o } -> std::string;
 };
 
-// ## Log LogRecord - Any item that can be printed in a Log Record
+/**
+ * @brief [items that are contained in a log record]
+ */
 enum log_properties {
-  FILE, // - File name (CodeContext)
-  FUNC, // - Function name (CodeContext)
-  LEVEL,
-  LINE, // - Line number (CodeContext)
-  HASH,
-  // NAME
-  DATE, // - Date a Log Record is created (RunContext)
-  TIME, // - Time a Log Record is created (RunContext)
-  THREAD,
+  FILE,           /// File name (CodeContext)
+  FUNC,           /// Function name (CodeContext)
+  LEVEL,          /// Severity level name (CodeContext)
+  LINE,           /// Line number (CodeContext)
 
-  MESG  // - Added by the user
-  // prop_time, prop_level, prop_thread, prop_file, prop_func, prop_line, prop_msg
+// NAME
+  DATE,           /// Date on which a Log Record is created (RunContext)
+  TIME,           /// Time at which a Log Record is created (RunContext)
+  THREAD,         /// Thread in which a LogRecord is created (RunContext)
+
+  HASH,
+  MESG            /// Added by the user
 };
 
-// ### Code Context - Log Properties that can be inferred from Code
+/**
+ * @brief Log properties that can be inferred from code.
+ * @details These can be used to answer 'where' in the code base is this log
+ * generated from.
+ */
 struct CodeContext {
   std::string file;
   std::string func;
@@ -43,50 +52,157 @@ struct CodeContext {
   int line;
   std::string hash;
 
+  /**
+   * @brief default constructs all the members
+   */
   CodeContext() = default;
-  // CodeContext(const std::string &a, const std::string &b, const std::string &c,
-  //           const int &d, const std::string &e)
-  //     : file(a), func(b), level(c), line(d), hash(e) {}
 
+  /**
+   * @brief Constructs the members from explicitly given values
+   * 
+   * @param a file name
+   * @param b function name
+   * @param c severity level
+   * @param d line number
+   * @param e hash value of the log, typically xor of memory locations of records
+   */
+  CodeContext(const std::string &a, const std::string &b, const std::string &c,
+            const int &d, const std::string &e)
+      : file(a), func(b), level(c), line(d), hash(e) {}
+
+  /**
+   * @brief Returns a tuple of all the members
+   * @details This returns a serialisable representation that can be used to
+   * print or order this class
+   * 
+   * @returns a tuple with all the constituents
+   */
   decltype(auto) tie() const { return std::tie(file, func, level, line, hash); }
+
+  /**
+   * @brief Compares two CodeContext using their tuple representation
+   * @details first compares file name, if same then compare function name,
+   * if same then level and so on.
+   * 
+   * @param rhs CodeContext to be compared with.
+   * @return if the lhs is smaller than the rhs.
+   */
   bool operator<(const CodeContext &rhs) const { return tie() < rhs.tie(); }
 };
 
+/**
+ * @brief prints CodeContext c to an out-stream ss
+ * 
+ * @param ss reference to a stream to which c should be printed
+ * @param c CodeContext to be printed to the stream
+ * @return reference to the stream to which c was printed
+ */
 std::ostream &operator<<(std::ostream &ss, const CodeContext &c) {
   return util::to_string(ss, c.tie());
 }
 
+/**
+ * @brief Log properties that are inferred when the code is run .
+ * @details These can be used to answer 'when' in the runtime was the log
+ * generated.
+ */
 struct RunContext {
   std::string date;
-  std::string time;
   std::string thread;
-  RunContext() = default;
-  // RunContext(const std::string &a, const std::string &b, const std::string &c)
-  //     : date(a), time(b), thread(c) {}
+  std::string time;
 
-  decltype(auto) tie() const { return std::tie(date, time, thread); }
+  /**
+   * @brief default constructs all the members
+   */
+  RunContext() = default;
+
+  /**
+   * @brief Constructs the members from explicitly given values
+   * 
+   * @param a date
+   * @param b time
+   * @param c thread identifier
+   */  
+  RunContext(const std::string &a, const std::string &b, const std::string &c)
+      : date(a), thread(b), time(c) {}
+
+
+  /**
+   * @brief Returns a tuple of all the members
+   * @details This returns a serialisable representation that can be used to
+   * print or order this class
+   * 
+   * @returns a tuple with all the constituents
+   */
+  decltype(auto) tie() const { return std::tie(date, thread, time); }
+
+  /**
+   * @brief Compares two RunContext using their tuple representation
+   * @details first compares date, if same then compare function thread,
+   * if same then time.
+   * 
+   * @param rhs RunContext to be compared with.
+   * @return if the lhs is smaller than the rhs.
+   */
   bool operator<(const RunContext &rhs) const { return tie() < rhs.tie(); }
 };
+
+
+/**
+ * @brief prints RunContext c to an out-stream ss
+ * 
+ * @param ss reference to a stream to which c should be printed
+ * @param c RunContext to be printed to the stream
+ * @return reference to the stream to which c was printed
+ */
 std::ostream &operator<<(std::ostream &ss, const RunContext &r) {
   return util::to_string(ss, r.tie());
 }
 
+/**
+ * @brief Each line in a log file can be considered a Log Record
+ * @details Object of this class contains all the information that can be
+ * retrieved from a log
+ */
 class LogRecord {
 public:
+
   using State = std::pair<CodeContext, RunContext>;
 
-  LogRecord() : code(){};
+  /**
+   * @brief default constructs all the members
+   */
+  LogRecord() = default;
 
+  /**
+   * @brief prints LogRecord c to an out-stream ss
+   * 
+   * @param ss reference to a stream to which c should be printed
+   * @param c LogRecord to be printed to the stream
+   * @return reference to the stream to which c was printed
+   */
   friend std::ostream &operator<<(std::ostream &s, const LogRecord &c);
 
-  CodeContext code;
-  RunContext run;
-  std::string message;
-  std::vector<float> numbers;
+  CodeContext code;               /// Code Context of the log record
+  RunContext run;                 /// Run Context of the log record
+  std::string message;            /// message of the log record
+  std::vector<float> numbers;     /// numbers in the message of the log record
 
+  /**
+   * @brief State of a log record is something that which you can use to 'Group By'
+   * @return the state of a log record
+   * @see typename State
+   */
   State get_state() { return std::make_pair(code, run); }
 
   // decltype(auto) tie() const { return std::tie(code, message); }
+
+
+
+  /**
+   * @details This returns a serialisable representation
+   * @returns a tuple of code context and run context
+   */
   decltype(auto) tie() const { return std::tie(code, run); }
 
 private:
@@ -95,8 +211,13 @@ private:
   // RunContext prog;
 };
 
-// ### Methods to read Properties for the analyser
-
+/**
+ * @brief reads the property (on which the function is templates) into
+ * a log record p from a string s
+ * 
+ * @param p log record to read into
+ * @param s string to read from
+ */
 template <log_properties N> void read_prop(LogRecord &p, const std::string &s);
 
 template <> void read_prop<FILE>(LogRecord &p, const std::string &s) {
@@ -144,6 +265,14 @@ void read_props(LogRecord &p, PS ps) {
   read_props<PS, I...>(p, ps);
 }
 
+/**
+ * @brief reads all the properties into a log record p from a string s
+ * @details reads only the properties on which the function is templates
+ * and in the order that the template appears in
+ * 
+ * @param p log record to read into
+ * @param s string to read from
+ */
 template <log_properties... I>
 void parse_props(LogRecord &p, std::string &line, 
     std::regex format_regex = std::regex("(.*)\\((.*):(.*)\\):(.*)")) {
@@ -175,6 +304,15 @@ std::ostream&  get_props<>(std::ostream& os, const LogRecord& rec){
     return os;
 }
 
+/**
+ * @brief gets all the properties from a log record p and streams to an outstream
+ * @details reads only the properties on which the function is templates
+ * and in the order that the template appears in
+ * 
+ * @param os outstream to stream to
+ * @param rec log record to get from
+ * @returns outstream that has been written to
+ */
 template<log_properties head, log_properties... I>
 std::ostream&  get_props(std::ostream& os, const LogRecord& rec) {
     os << get_prop<head>(rec)<<" ";
@@ -182,18 +320,31 @@ std::ostream&  get_props(std::ostream& os, const LogRecord& rec) {
     return os;
 }
 
-std::ostream& get_props_test(std::ostream& os, const LogRecord& rec) {
-  return get_props<FILE, FUNC>(os, rec);
+// std::ostream& get_props_test(std::ostream& os, const LogRecord& rec) {
+//   return get_props<FILE, FUNC>(os, rec);
+// }
+
+
+/**
+ * @brief streams a log record to an outstream
+ * 
+ * @param os outstream to stream to
+ * @param lr log record to stream
+ */
+std::ostream &operator<<(std::ostream &os, const LogRecord &lr) {
+  return util::to_string(os, lr.tie()) << lr.message;
 }
 
-std::ostream &operator<<(std::ostream &ss, const LogRecord &lr) {
-  return util::to_string(ss, lr.tie()) << lr.message;
-}
-
-std::ostream &operator<<(std::ostream &s,
+/**
+ * @brief streams the state of a log record to an outstream
+ * 
+ * @param os outstream to stream to
+ * @param p state of a log record
+ */
+std::ostream &operator<<(std::ostream &os,
                           const std::pair<CodeContext, RunContext> &p){
 
-  return util::to_string(s, std::tuple<CodeContext, RunContext>(p));
+  return util::to_string(os, std::tuple<CodeContext, RunContext>(p));
 }
 }
 
